728943c1f9b2258699ac2339d173c07c9edbe93f
KevinSinatria <kevinsinatria8@gmail.com>
2025-10-12 13:22:50 +0700

    chore(deployment): change start port

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const apiClient = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? BASE_URL : "/api",
+  baseURL: process.env.NEXT_PUBLIC_NODE_ENV === "production" ? BASE_URL : "/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
